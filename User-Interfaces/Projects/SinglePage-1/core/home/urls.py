be626d45080a6c0137701b9d582c0f9fe7539991
lashkaryrad <lashkaryrad@gmail.com>
2023-07-03 21:58:13 +0330

    Adding Some Spaces

--- User-Interfaces/Projects/SinglePage-1/core/home/urls.py
+++ User-Interfaces/Projects/SinglePage-1/core/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('section/<int:num>/', views.section, name='section'),
     #Get num for posting to local API 
     #and getting Content in /section/num
