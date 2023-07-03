dc363d9487adc17b11afd5d45a0a537a9f7619af
AmirArsalan <amirarsalan@MacBook-Air.local>
2023-07-03 19:27:12 +0330

    get button's dataset: page;

--- User-Interfaces/js/singlePage-src.js
+++ User-Interfaces/js/singlePage-src.js
@@ -1,1 +1,14 @@
-console.log(true)
+// Get Buttons DataSet
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Access To all Button's DataSet
+    document.querySelectorAll("button").forEach((button) => {
+
+        // if Button clicked:
+        button.onclick = function() {
+            console.log(this.dataset.page)
+        }
+    })
+
+    return false; // Sent data as Ajax.
+})
